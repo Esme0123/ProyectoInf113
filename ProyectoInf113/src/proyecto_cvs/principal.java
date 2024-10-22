88714fece216ec6c02677fa7dc0d66489ab1b0f4
FelipeMejia12 <carlo.mejia@ucb.edu.bo>
2024-10-22 15:38:58 -0400

    Prueba de otro cambio

--- ProyectoInf113/src/proyecto_cvs/principal.java
+++ ProyectoInf113/src/proyecto_cvs/principal.java
@@ -8,6 +8,7 @@ public class principal {
 		//c1.conectar();
 		cliente cl1=new cliente();
 		cl1.setApellido("Mejia");
+		cl1.setCelular(1123123);
 		System.out.print(cl1.getApellido());
 		//prueba
 	}
