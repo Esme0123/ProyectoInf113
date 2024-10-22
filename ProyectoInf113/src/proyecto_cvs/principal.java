4dae8f779865b0142c4fc0a77744130203bfd433
Felipe Mejia <Felipe Mejia@DESKTOP-9U0O34J>
2024-10-22 15:19:22 -0400

    Prueba para subir

--- ProyectoInf113/src/proyecto_cvs/principal.java
+++ ProyectoInf113/src/proyecto_cvs/principal.java
@@ -5,7 +5,11 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		conexion c1=new conexion();
-		c1.conectar();
+		//c1.conectar();
+		cliente cl1=new cliente();
+		cl1.setApellido("Mejia");
+		System.out.print(cl1.getApellido());
+		//prueba
 	}
 
 }
