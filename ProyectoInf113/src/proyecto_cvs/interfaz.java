89cb555ad06bcfd122f093f128cb9d83e8cf01f2
Esmeralda Medina <esmeralda.medina.p@ucb.edu.bo>
2024-11-19 22:34:58 -0400

    prueba interfaz

--- ProyectoInf113/src/proyecto_cvs/interfaz.java
+++ ProyectoInf113/src/proyecto_cvs/interfaz.java
@@ -5,11 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class interfaz extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -37,6 +44,25 @@ public class interfaz extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(69, 123, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnCliente = new JButton("Registar");
+		btnCliente.setBounds(274, 68, 89, 23);
+		contentPane.add(btnCliente);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(104, 172, 5, 22);
+		contentPane.add(textArea);
+		
+		JLabel nlbTitulo = new JLabel("RESTAURANTE");
+		nlbTitulo.setFont(new Font("Mistral", Font.BOLD, 20));
+		nlbTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		nlbTitulo.setBounds(157, 11, 128, 34);
+		contentPane.add(nlbTitulo);
 	}
-
 }
